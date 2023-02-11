0662867df7375c563712c9b8969ccafd0c94fb01
Timna Schulze <Timna.lansel@gmail.com>
2023-02-10 20:09:20 -0800

    create HTML and CSS styling for application

---
+++ js/styles.js
@@ -0,0 +1,18 @@
+//User Interface Logic
+
+function hideResults() {
+  document.getElementById("roboLanguage").setAttribute("class", "hidden");
+}
+
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+  event.preventDefault();
+  };
+};
+
+//Business Logic
+
+button.onclick = function() {
+
+}
