12d8d8bd55f1e9f97e1bdca4237ff661988ed21d
Timna Schulze <Timna.lansel@gmail.com>
2023-02-11 18:53:28 -0800

    WIP: tries to get any function output to display for the submit button

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,31 @@ form.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
   event.preventDefault();
-
-document.getElementById("submit").addAttribute("class", "hidden");
-
-function roboLanguage() {
-  let numberInput = document.querySelector("userNumber").value;
   };
-document.getElementById("submit").innerHTML= "Mr. Roboger says" + numberInput + " .";
+};
 
-document.getElementById("submit").removeAttribute("class","hidden");
+document.getElementById("submit").addAttribute("class","hidden");
+
+//Business Logic
+
+//input userNumber to print array of counting from 0 to userNumber
+function roboLanguage() {
+  const userNum = document.getElementById("userNumber");
+  let numListArr = [];
+
+  numListArr.forEach(function(element) {
+    let numCount=0;
+    for (let i=0; i <=userNum; i++) {
+      if (element === numList[i]) {
+        numCount++;
+      }
+    }
+    numListArr.push(element + numCount);
+  });
+
+  document.getElementById("submit").innerHTML= "Mr. Roboger says" + numListArr + " .";
+
+  document.getElementById("submit").removeAttribute("class","hidden");
 };
 
 document.getElementById("tryAgain").addEventListener("click", function() {
@@ -19,5 +35,14 @@ document.getElementById("tryAgain").addEventListener("click", function() {
   form.reset();
 });
 
-//Business Logic
+
+
+
+
+
+
+
+
+
+
 
